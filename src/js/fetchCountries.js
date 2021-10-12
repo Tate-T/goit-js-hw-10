c90c299202bea6c339c4327672cfa1581e903227
Tate-T <tatjana.tarasovych@gmail.com>
2021-10-12 14:43:34 +0300

    corrected fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,7 +4,7 @@ import Notiflix from 'notiflix';
 export const fetchCountries = (inputData) => {
     return fetch(`${baseUrl}${inputData}?fields=name,capital,population,flag,languages`)
         .then(response => {
-            if (response.status >= 200 && response.status < 300) {
+            if (!(response.status >= 200) && !(response.status < 300)) {
                 throw new Error(response.status);
             }
             return response.json();
